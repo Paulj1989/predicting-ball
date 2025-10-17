0bb47ddd736b5d5b0a92c44b05c1e5a93c4969be
paulj1989 <paul@paulrjohnson.net>
2025-10-17 22:52:07 +0100

    Fix tracking

--- app/components/analytics.py
+++ app/components/analytics.py
@@ -5,9 +5,8 @@ import streamlit as st
 
 def umami_tracker():
     """Inject Umami analytics tracking script"""
-    st.markdown(
+    st.html(
         """
         <script defer src="https://cloud.umami.is/script.js" data-website-id="8de8dbc1-d49c-49a1-a42a-05916ad3e1a7"></script>
-        """,
-        unsafe_allow_html=True,
+        """
     )
